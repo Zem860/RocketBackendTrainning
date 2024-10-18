39288ecf38c99d0704ec92b448c93933e817f328
Zem860 <zemmyhammy@gmail.com>
2024-10-18 10:25:56 +0800

    寫到4-3

--- BackendHomework/Program.cs
+++ BackendHomework/Program.cs
@@ -14,21 +14,57 @@ namespace BackendHomework
         {
 
             //4-1利用while寫一程式求N個數字的最大值。
-            Console.Write("輸入範圍:");
-            int range = Convert.ToInt32(Console.ReadLine());
-            int i = 0;
-            int maxNum = int.MinValue;
-            while (i < range)
+            //Console.Write("輸入範圍:");
+            //int range = Convert.ToInt32(Console.ReadLine());
+            //int i = 0;
+            //int maxNum = int.MinValue;
+            //while (i < range)
+            //{
+            //    Console.Write($"數字{i+1}為:");
+            //    int num = Convert.ToInt32(Console.ReadLine());
+            //    if (num > maxNum)
+            //    {
+            //        maxNum = num;
+            //    }
+            //    i++;
+            //}
+            //Console.WriteLine($"最大的數字為{maxNum}");
+
+            //4-2 求等差級數數字的和
+            //int startValue, n, difference, count = 0, sum = 0;
+            //Console.Write("請輸入理想範圍: ");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("請輸入起始值: ");
+            //startValue = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("請輸入公差:");
+            //difference = Convert.ToInt32(Console.ReadLine());
+            //while (count < n)
+            //{
+            //    sum += startValue;
+            //    startValue += difference;
+            //    count++;
+            //}
+
+            //Console.Write($"等差級數數字的和{sum}");
+            //4-3  利用while寫一程式，讀入N個數字，然後找出所有小於13的數，再求這些數字的和。
+
+            Console.Write("請輸入N個數字:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int temp = 0;
+            
+            while (n > 0)
             {
-                Console.Write($"數字{i+1}為:");
-                int num = Convert.ToInt32(Console.ReadLine());
-                if (num > maxNum)
+                Console.WriteLine("請輸入隨機數字:");
+                int rndNum = Convert.ToInt32(Console.ReadLine());
+                if (rndNum < 13)
                 {
-                    maxNum = num;
+                    temp += rndNum;
                 }
-                i++;
+
+                n--;
             }
-            Console.WriteLine($"最大的數字為{maxNum}");
+            Console.WriteLine($"所有小於13的數字的和{temp}");
             Console.ReadKey();
         }
     }
