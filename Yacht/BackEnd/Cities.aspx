7341ac709d5b6b8652cb123691cc4adbbf5b05f1
Zem860 <zemmyhammy@gmail.com>
2025-02-06 00:18:54 +0800

    feat: add CityPage

---
+++ Yacht/BackEnd/Cities.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BackEnd/Dashboard.Master" AutoEventWireup="true" CodeBehind="Cities.aspx.cs" Inherits="Yacht.BackEnd.Cities" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Countries" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Dealers" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="EditDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="AddDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="Cities" runat="server">
+    <script>
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+    </script>
+
+    <!-- 國家選單 -->
+    <p>Country</p>
+    <asp:RadioButtonList ID="Countries" runat="server" AutoPostBack="True" OnSelectedIndexChanged="Countries_SelectedIndexChanged"></asp:RadioButtonList>
+    <p>City</p>
+    <div id="addArea">
+        <asp:TextBox ID="CityName" runat="server"></asp:TextBox>
+        <asp:Button ID="AddBtn" runat="server" Text="Add City" OnClick="addCity" />
+    </div>
+
+    <!-- 城市選單 -->
+    <asp:GridView ID="CityGridView" runat="server" AutoGenerateColumns="false">
+        <Columns>
+            <asp:TemplateField HeaderText="CityName">
+                <ItemTemplate>
+                    <%# Eval("City") %></a>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Ops">
+                <ItemTemplate>
+                    <asp:Button ID="btnEdit" runat="server" Text="Edit" CommandName="Edit" />
+                    <asp:Button ID="btnDelete" OnClientClick="return confirm('Are you sure you want to delete？')" runat="server" Text="Delete" CommandName="Delete" />
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:Button ID="btnSave" runat="server" Text="Save" CommandName="Update" />
+                    <asp:Button ID="btnCancel" runat="server" Text="Cancel" CommandName="Cancel" />
+                </EditItemTemplate>
+                 </asp:TemplateField>
+        </Columns>
+
+    </asp:GridView>
+
+</asp:Content>
---
+++ Yacht/BackEnd/Cities.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Yacht.BackEnd
+{
+    public partial class Cities : System.Web.UI.Page
+    {
+        protected string connectionString = WebConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                showCountries();
+            }
+        }
+
+        public void showCountries()
+        {
+            string query = @"SELECT Id, CountryName FROM Countries";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                Countries.DataSource = reader;
+                Countries.DataTextField = "CountryName";
+                Countries.DataValueField = "Id";
+                Countries.SelectedValue = "1";
+                Countries.DataBind();
+            }
+            showCities();
+
+
+        }
+        public void showCities()
+        {    
+            string query = @"SELECT Id, City FROM Cities WHERE CountryId = @Id";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue(@"Id", Countries.SelectedValue);
+                SqlDataReader reader = cmd.ExecuteReader();
+                CityGridView.DataSource = reader;
+                CityGridView.DataBind();
+            }
+        }
+
+        protected void Countries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            showCities();
+        }
+
+        protected void addCity(object sender, EventArgs e)
+        {
+            string query = @"INSERT INTO Cities (CountryId, City) VALUES(@countryId,@city)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue(@"countryId", Countries.SelectedValue);
+                cmd.Parameters.AddWithValue(@"city", CityName.Text);
+                cmd.ExecuteNonQuery();
+            }
+            showCities();
+        }
+    }
+}
