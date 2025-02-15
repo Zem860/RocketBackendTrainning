d15bf805cb0f596e41f86ac739485fe80aff5cc4
Zem860 <zemmyhammy@gmail.com>
2025-02-15 19:34:03 +0800

    feat: add backend add yachtModel add page

---
+++ Yacht/BackEnd/AddYachtModels.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BackEnd/Dashboard.Master" AutoEventWireup="true" CodeBehind="AddYachtModels.aspx.cs" Inherits="Yacht.BackEnd.YachtModels" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Default" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Countries" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="Dealers" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="EditDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="AddDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content7" ContentPlaceHolderID="Cities" runat="server">
+</asp:Content>
+<asp:Content ID="Content8" ContentPlaceHolderID="AddNews" runat="server">
+</asp:Content>
+<asp:Content ID="Content9" ContentPlaceHolderID="News" runat="server">
+</asp:Content>
+<asp:Content ID="Content10" ContentPlaceHolderID="EditNews" runat="server">
+</asp:Content>
+<asp:Content ID="Content11" ContentPlaceHolderID="EditNewsPhoto" runat="server">
+</asp:Content>
+<asp:Content ID="Content12" ContentPlaceHolderID="EditNewsFile" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content13" ContentPlaceHolderID="AddYachtsModel" runat="server">
+    <div class="container w-75 d-flex justify-content-center flex-column p-2">
+        <div class="d-flex gap-5 justify-content-center align-items-center">
+            <p>Yacht Model</p>
+            <asp:TextBox CssClass="w-25" ID="YachtModel" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="d-flex gap-5 justify-content-center align-items-center">
+            <p>Latest Yacht Model</p>
+            <asp:CheckBox ID="IsNewModel" runat="server" />
+        </div>
+
+        <div class="d-flex gap-5 justify-content-center align-items-center">
+            <p>Ship Photos</p>
+            <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="true" />
+        </div>
+
+        <asp:Button ID="Submit" runat="server" Width="100" CssClass="btn btn-primary rounded-2 align-content-lg-center"
+            Text="Submit" OnClick="addModel" />
+    </div>
+</asp:Content>
+
---
+++ Yacht/BackEnd/AddYachtModels.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Yacht.BackEnd
+{
+    public partial class YachtModels : System.Web.UI.Page
+    {
+        protected string connectionString = WebConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void addShipModel()
+        {
+            if (!FileUpload1.HasFiles)
+            {
+                Response.Write("<script>alert('Please upload at least one photo');</script>");
+                return;
+            }
+            else if (String.IsNullOrEmpty(YachtModel.Text))
+            {
+                Response.Write("<script>alert('Please enter a Yacht Model');</script>");
+                return;
+            }
+
+            // 確認是否為最新船型（CheckBox）
+            int isNewValue = IsNewModel.Checked ? 1 : 0;
+
+            string query = @"INSERT INTO YachtsModel (Model, IsNew, CreatedAt, UpdatedAt) 
+                             VALUES (@model, @isNew, GETDATE(), GETDATE()); 
+                             SELECT SCOPE_IDENTITY();";
+
+            int dataId;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue(@"model", YachtModel.Text);
+                cmd.Parameters.AddWithValue(@"isNew", isNewValue);
+
+                object shipId = cmd.ExecuteScalar();
+                dataId = (shipId != null) ? Convert.ToInt32(shipId) : 0;
+            }
+
+            // 新增照片
+            addPhotos(dataId);
+        }
+
+        public void addPhotos(int shipId)
+        {
+            string localPath = Server.MapPath("~/ShipImages/");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                foreach (var img in FileUpload1.PostedFiles)
+                {
+                    int imgMemory = img.ContentLength;
+                    string imgName = Path.GetFileName(img.FileName);
+                    string imgExtension = Path.GetExtension(img.FileName);
+                    string localSavingPath = Path.Combine(localPath, imgName);
+
+                    if (imgMemory > 1000000)
+                    {
+                        continue;
+                    }
+                    else if (imgExtension != ".jpg" && imgExtension != ".png")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        string mappingPath = "/ShipImages/" + imgName;
+                        string query = @"INSERT INTO YachtImgs (YachtId, ImgPath, CreatedAt) 
+                                         VALUES (@shipId, @imgPath, GETDATE())";
+
+                        SqlCommand cmd = new SqlCommand(query, connection);
+                        cmd.Parameters.AddWithValue(@"shipId", shipId);
+                        cmd.Parameters.AddWithValue(@"imgPath", mappingPath);
+                        cmd.ExecuteNonQuery();
+
+                        img.SaveAs(localSavingPath);
+                    }
+                }
+            }
+        }
+
+        protected void addModel(object sender, EventArgs e)
+        {
+            addShipModel();
+        }
+    }
+}
