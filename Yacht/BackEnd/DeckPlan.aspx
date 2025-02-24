f3c796714d7016584d46575ffeffe5eb813b2143
Zem860 <zemmyhammy@gmail.com>
2025-02-24 23:45:29 +0800

    feat: add Deckplan page

---
+++ Yacht/BackEnd/DeckPlan.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BackEnd/Dashboard.Master" AutoEventWireup="true" CodeBehind="DeckPlan.aspx.cs" Inherits="Yacht.BackEnd.DeckPlan" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Default" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Countries" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="Dealers" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="EditDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="AddDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content7" ContentPlaceHolderID="Cities" runat="server">
+</asp:Content>
+<asp:Content ID="Content8" ContentPlaceHolderID="AddNews" runat="server">
+</asp:Content>
+<asp:Content ID="Content9" ContentPlaceHolderID="News" runat="server">
+</asp:Content>
+<asp:Content ID="Content10" ContentPlaceHolderID="EditNews" runat="server">
+</asp:Content>
+<asp:Content ID="Content11" ContentPlaceHolderID="EditNewsPhoto" runat="server">
+</asp:Content>
+<asp:Content ID="Content12" ContentPlaceHolderID="EditNewsFile" runat="server">
+</asp:Content>
+<asp:Content ID="Content13" ContentPlaceHolderID="AddYachtsModel" runat="server">
+</asp:Content>
+<asp:Content ID="Content14" ContentPlaceHolderID="AddYachtSpec" runat="server">
+</asp:Content>
+<asp:Content ID="Content15" ContentPlaceHolderID="Specification" runat="server">
+</asp:Content>
+<asp:Content ID="Content16" ContentPlaceHolderID="YachtsModel" runat="server">
+</asp:Content>
+<asp:Content ID="Content17" ContentPlaceHolderID="EditShipPhotos" runat="server">
+</asp:Content>
+<asp:Content ID="Content18" ContentPlaceHolderID="DeckPlan" runat="server">
+    <div class="container mt-4">
+        <div class="mb-3">
+            <label for="YachtModel" class="form-label fw-bold">Select Yacht Model</label>
+            <asp:DropDownList ID="YachtModel" AutoPostBack="true" runat="server" CssClass="form-select" OnSelectedIndexChanged="YachtModel_SelectedIndexChanged"></asp:DropDownList>
+        </div>
+        <div class="card shadow-sm p-4">
+            <h5 class="mb-3">Upload Related Files</h5>
+            <div class="mb-3">
+                <asp:FileUpload ID="ImgUpload" runat="server" AllowMultiple="True" CssClass="form-control" />
+            </div>
+            <div class="d-flex justify-content-end">
+                <asp:Button ID="Submit" runat="server" Text="Upload" CssClass="btn btn-primary" OnClick="addImg" />
+            </div>
+        </div>
+
+        <!-- 已上傳圖片列表 -->
+        <div class="card mt-4 shadow-sm">
+            <div class="card-header bg-dark text-white">
+                <h5 class="mb-0">Uploaded Files</h5>
+            </div>
+            <div class="card-body">
+                <asp:GridView AutoGenerateColumns="false" DataKeyNames="Id" ID="DeckGridView" runat="server"
+                    CssClass="table table-bordered table-hover text-center">
+                    <Columns>
+                        <asp:TemplateField HeaderText="Select">
+                            <ItemTemplate>
+                                <asp:CheckBox ID="chkSelect" runat="server" CssClass="form-check-input" />
+                             <asp:HiddenField ID="hiddenId" runat="server" Value='<%# Eval("Id") %>' />
+
+                            </ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="File Name">
+                            <ItemTemplate>
+                                <img class="img-fluid rounded shadow-sm" style="max-width: 250px;" src="<%# Eval("ImgPath") %>" alt="Image" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+
+        <!-- 刪除按鈕 -->
+        <div class="d-flex justify-content-end mt-3">
+            <asp:Button ID="btnDeleteSelected" runat="server" CssClass="btn btn-danger" 
+                Text="Delete Selected" OnClick="btnDeleteSelected_Click" 
+                OnClientClick="return confirm('Are you sure you want to delete selected items?')" />
+        </div>
+    </div>
+</asp:Content>
+
---
+++ Yacht/BackEnd/DeckPlan.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Yacht.BackEnd
+{
+    public partial class DeckPlan : System.Web.UI.Page
+    {
+        protected string connectionString = WebConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) // 只在首次載入時執行
+            {
+                getDropDown();
+                getDeckImg(); // 載入預設的船型圖片
+            }
+        }
+
+
+        protected void addImg(object sender, EventArgs e)
+        {
+            string localPath = Server.MapPath("~/DeckPlanImgs/");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                if (ImgUpload.HasFiles)
+                {
+                    foreach (var img in ImgUpload.PostedFiles)
+                    {
+                        int imgMemory = img.ContentLength;
+                        string FileName = Path.GetFileName(img.FileName);
+                        string imgExtension = Path.GetExtension(img.FileName);
+                        string localStorePath = Path.Combine(localPath, FileName);
+                        if (imgMemory > 1000000)
+                        {
+                            continue;
+                        }
+                        else if (imgExtension != ".jpg" && imgExtension != ".png")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            string localMappingPath = "/DeckPlanImgs/" + FileName;
+                            string query = @"INSERT INTO LayoutImg (yachtId, ImgPath) VALUES (@yachtId, @ImgPath)";
+                            SqlCommand cmd = new SqlCommand(query, connection);
+                            cmd.Parameters.Clear();
+                            cmd.Parameters.AddWithValue(@"yachtId", YachtModel.SelectedValue);
+                            cmd.Parameters.AddWithValue(@"ImgPath", localMappingPath);
+                            cmd.ExecuteNonQuery();
+                            img.SaveAs(localStorePath);
+                        }
+                        getDeckImg();
+
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Please add at least one photo')</script>");
+                }
+            }
+        }
+        public void getDropDown()
+        {
+            string query = @"SELECT Id, Model FROM YachtsModel";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                YachtModel.DataSource = reader;
+                YachtModel.DataTextField = "Model";  // 設定 Model 為顯示的名稱
+                YachtModel.DataValueField = "Id";    // 設定 Id 為選擇的值
+                YachtModel.DataBind();
+            }
+        }
+
+        public void getDeckImg()
+        {
+            string query = @"SELECT Id, ImgPath FROM LayoutImg WHERE YachtId = @id";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue(@"id", YachtModel.SelectedValue);
+                SqlDataReader reader = cmd.ExecuteReader();
+                DeckGridView.DataSource = reader;
+                DeckGridView.DataBind();
+
+            }
+        }
+
+        protected void btnDeleteSelected_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                foreach (GridViewRow row in DeckGridView.Rows)
+                {
+                    CheckBox chk = (CheckBox)row.FindControl("chkSelect");
+                    HiddenField hiddenId = (HiddenField)row.FindControl("hiddenId");
+
+                    if (chk != null && chk.Checked && hiddenId != null)
+                    {
+                        int id = Convert.ToInt32(hiddenId.Value);
+                        string query = @"DELETE FROM LayoutImg WHERE Id = @Id";
+                        using (SqlCommand cmd = new SqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@Id", id);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+                getDeckImg();
+            }
+        }
+
+        protected void YachtModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            getDeckImg();
+        }
+    }
+}
