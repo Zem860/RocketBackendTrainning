d8350ed041db86f3218a2e61a735bc0a01427ac9
Zem860 <zemmyhammy@gmail.com>
2025-02-25 11:11:53 +0800

    update: change backend Master layout

--- Yacht/BackEnd/Dashboard.Master.cs
+++ Yacht/BackEnd/Dashboard.Master.cs
@@ -10,47 +10,47 @@ namespace Yacht.BackEnd
 {
     public partial class Dashboard1 : System.Web.UI.MasterPage
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            //權限關門判斷 (Cookie)
-            if (!HttpContext.Current.User.Identity.IsAuthenticated)
-            {
-                Response.Redirect("Login.aspx"); //導回登入頁
-            }
-            else
-            {
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    //權限關門判斷 (Cookie)
+        //    if (!HttpContext.Current.User.Identity.IsAuthenticated)
+        //    {
+        //        Response.Redirect("Login.aspx"); //導回登入頁
+        //    }
+        //    else
+        //    {
 
-                FormsIdentity identity = HttpContext.Current.User.Identity as FormsIdentity;
-                FormsAuthenticationTicket ticket = identity.Ticket;
+        //        FormsIdentity identity = HttpContext.Current.User.Identity as FormsIdentity;
+        //        FormsAuthenticationTicket ticket = identity.Ticket;
 
-                // 從票證中取得儲存的 UserData (這裡就是使用者帳號)
-                string userAccount = ticket.UserData;
+        //        // 從票證中取得儲存的 UserData (這裡就是使用者帳號)
+        //        string userAccount = ticket.UserData;
 
-                // 將使用者帳號顯示在 Label 上
-                UserName.Text = userAccount;
-            }
+        //        // 將使用者帳號顯示在 Label 上
+        //        UserName.Text = userAccount;
+        //    }
 
-        }
+        //}
 
-        protected void Logout_Click(object sender, EventArgs e)
-        {
-            // 清除 Forms 認證的 Cookie
-            FormsAuthentication.SignOut();
+        //protected void Logout_Click(object sender, EventArgs e)
+        //{
+        //    // 清除 Forms 認證的 Cookie
+        //    FormsAuthentication.SignOut();
 
-            // 清除 Session
-            Session.Clear();
-            Session.Abandon();
+        //    // 清除 Session
+        //    Session.Clear();
+        //    Session.Abandon();
 
-            // 清除認證 Cookie
-            HttpCookie authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, "")
-            {
-                Expires = DateTime.Now.AddDays(-1), // 立即過期
-                HttpOnly = true
-            };
-            Response.Cookies.Add(authCookie);
+        //    // 清除認證 Cookie
+        //    HttpCookie authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, "")
+        //    {
+        //        Expires = DateTime.Now.AddDays(-1), // 立即過期
+        //        HttpOnly = true
+        //    };
+        //    Response.Cookies.Add(authCookie);
 
-            // 重新導向回登入頁
-            Response.Redirect("~/Login.aspx");
-        }
+        //    // 重新導向回登入頁
+        //    Response.Redirect("~/Login.aspx");
+        //}
     }
 }
