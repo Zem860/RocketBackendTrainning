d1fa04234d93d6bd0155c3aef21807e6966cca37
Zem860 <zemmyhammy@gmail.com>
2025-02-25 11:57:35 +0800

    feat:add NewsFile page

---
+++ Yacht/BackEnd/EditNewsFile.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BackEnd/Dashboard.Master" AutoEventWireup="true" CodeBehind="EditNewsFile.aspx.cs" Inherits="Yacht.BackEnd.EditNewsFile" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Default" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Countries" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="Dealers" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="EditDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="AddDealer" runat="server">
+</asp:Content>
+<asp:Content ID="Content7" ContentPlaceHolderID="Cities" runat="server">
+</asp:Content>
+<asp:Content ID="Content8" ContentPlaceHolderID="AddNews" runat="server">
+</asp:Content>
+<asp:Content ID="Content9" ContentPlaceHolderID="News" runat="server">
+</asp:Content>
+<asp:Content ID="Content10" ContentPlaceHolderID="EditNews" runat="server">
+</asp:Content>
+<asp:Content ID="Content11" ContentPlaceHolderID="EditNewsPhoto" runat="server">
+</asp:Content>
+<asp:Content ID="Content12" ContentPlaceHolderID="EditNewsFile" runat="server">
+            <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+                }
+            </script>
+    <div class="container mt-5">
+        <!-- Row: 容納列元素 -->
+        <div class="row d-flex justify-content-center border-1 border-dark p-3">
+            <!-- Col: 占 12 列中的 6 列，畫面小於768px時會自動換行 -->
+            <div class="col-md-6">
+                <!-- News Title -->
+                <div class="mb-3">
+                    <label for="NewsTitle" class="form-label">News Title</label>
+                    <asp:TextBox ID="NewsTitle" Enabled="false" runat="server" CssClass="form-control" />
+                </div>
+                <!-- News Photos -->
+                <div class="mb-3">
+                    <p>FileNames</p>
+                    <asp:GridView AutoGenerateColumns="false" DataKeyNames="FileId" ID="FileGridView" runat="server" OnRowDeleting="Delete">
+                        <Columns>
+                            <asp:TemplateField HeaderText="FileName">
+                                <ItemTemplate>
+                                    <p class=" text-info"><%# Eval("FileName") %></a></p>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Delete">
+                                <ItemTemplate>
+                                    <asp:Button ID="btnDelete" CssClass="btn btn-danger" OnClientClick="return confirm('Are you sure you want to delete？')" runat="server" Text="Delete" CommandName="Delete" />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+
+                </div>
+                    <div class="mb-3">
+                        <label for="FileUpload2" class="form-label">Related Files</label>
+                        <asp:FileUpload ID="FileUpload2" runat="server" AllowMultiple="True" CssClass="form-control" />
+                    </div>
+                <!-- Submit Button -->
+                <div class="mb-3">
+                    <asp:Button ID="Button1" runat="server" Text="Submit" CssClass="btn btn-primary" OnClick="AddFiles" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Yacht/BackEnd/EditNewsFile.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Yacht.BackEnd
+{
+    public partial class EditNewsFile : System.Web.UI.Page
+    {
+        protected string Id = string.Empty;
+        protected string connectionString = WebConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                Id = Request.QueryString["Id"];
+                if (String.IsNullOrEmpty(Id))
+                {
+                    Response.Redirect("News.aspx");
+                }
+                show();
+            }
+        }
+        protected void AddFiles(object sender, EventArgs e)
+        {
+            if (FileUpload2.HasFile)
+            {
+                string localPathHeading = Server.MapPath("~/NewsFiles/");
+                string query = @"INSERT INTO NewsFiles (NewsId, FileName, FilePath) VALUES (@id, @name,@path)";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    foreach (var file in FileUpload2.PostedFiles)
+                    {
+                        int fileMemory = file.ContentLength;
+                        string fileName = Path.GetFileName(file.FileName);
+                        string imgExtension = Path.GetExtension(file.FileName).ToLower();
+                        string localPath = Path.Combine(localPathHeading, fileName);
+                        if (fileMemory > 1000000)
+                        {
+                            continue;
+                        }
+                        else if (imgExtension != ".pdf" && imgExtension != ".txt")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            string fileMappingPath = "/NewsFiles/" + fileName;
+                            cmd.Parameters.Clear();
+                            cmd.Parameters.AddWithValue(@"id", Request.QueryString["Id"]);
+                            cmd.Parameters.AddWithValue(@"path", fileMappingPath);
+                            cmd.Parameters.AddWithValue(@"name", fileName);
+                            file.SaveAs(localPath);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    show();
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('You need to attach at least one file!')</script>");
+            }
+        }
+
+        public void getTitle()
+        {
+            string query = @"SELECT Title FROM News WHERE Id =@id";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue(@"id", Id);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    NewsTitle.Text = reader["Title"].ToString();
+                }
+            }
+        }
+
+        public void show()
+            
+        {
+
+            string query = @"SELECT Id AS FileId, FileName AS FileName
+                                FROM NewsFiles                             
+                                WHERE NewsId = @id";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue(@"id", Request.QueryString["Id"]);
+                SqlDataReader reader = cmd.ExecuteReader();
+                FileGridView.DataSource = reader;
+                FileGridView.DataBind();
+            }
+            getTitle();
+        }
+
+        protected void Delete(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(FileGridView.DataKeys[e.RowIndex].Value);
+            string query = @"DELETE FROM NewsFiles WHERE Id = @id";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue(@"id", id);
+                cmd.ExecuteNonQuery();
+
+            }
+            show();
+        }
+    }
+}
